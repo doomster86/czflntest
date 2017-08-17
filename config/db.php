2337f3239c89eebe940b7e8b5fea0a7358a3c45b
doomster <at86@yandex.ru>
2017-08-17 11:36:21 +0300

    testing db

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=93.158.209.70;dbname=derg',
-    'username' => 'czuser',
-    'password' => 'E8b1L3c0',
+    'username' => 'doomster',
+    'password' => 'Q9g1F5v4',
     'charset' => 'utf8',
 ];
