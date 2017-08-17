8780a327445b39500384364e3ed237c1cd2c95d8
doomster <at86@yandex.ru>
2017-08-17 12:14:14 +0300

    testing db

--- config/db.php
+++ config/db.php
@@ -1,8 +1,16 @@
 <?php
 
+/* return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+]; */
+
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=93.158.209.70;dbname=derg',
+    'dsn' => 'mysql:host=93.158.209.70;port=1315;dbname=derg',
     'username' => 'doomster',
     'password' => 'Q9g1F5v4',
     'charset' => 'utf8',
