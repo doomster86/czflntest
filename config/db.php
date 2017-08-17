e4f49d1d02b1046fb2eb7876aea1a3c050351f4b
doomster <at86@yandex.ru>
2017-08-17 12:24:50 +0300

    testing local db

--- config/db.php
+++ config/db.php
@@ -1,17 +1,9 @@
 <?php
 
-/* return [
+return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2basic',
     'username' => 'root',
     'password' => '',
-    'charset' => 'utf8',
-]; */
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=93.158.209.70;port=1315;dbname=derg',
-    'username' => 'doomster',
-    'password' => 'Q9g1F5v4',
     'charset' => 'utf8',
 ];
