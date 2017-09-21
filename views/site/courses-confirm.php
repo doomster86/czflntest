627625fe80035c141947e3924e131e82eaa4038b
doomster <at86@yandex.ru>
2017-09-21 14:53:04 +0300

    courses 2

--- views/site/courses-confirm.php
+++ views/site/courses-confirm.php
@@ -6,4 +6,7 @@ use yii\helpers\Html;
 <ul>
     <li><label>Название курса: </label>: <?= Html::encode($model->name) ?></li>
     <li><label>Преподаватель курса</label>: <?= Html::encode($model->teacher) ?></li>
+    <li><label>Преподаватель курса</label>: <?= Html::encode($model->group) ?></li>
+    <li><label>Преподаватель курса</label>: <?= Html::encode($model->lections) ?></li>
+    <li><label>Преподаватель курса</label>: <?= Html::encode($model->practics) ?></li>
 </ul>
