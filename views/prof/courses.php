ba5f0d9d062a489b692d2ff4623f7c8dcf102ad8
doomster <at86@yandex.ru>
2017-10-31 14:21:43 +0200

    full form

--- views/prof/courses.php
+++ views/prof/courses.php
@@ -12,7 +12,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Створити професію', ['courses-create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Створити нову', ['courses-create'], ['class' => 'btn btn-success']) ?>
     </p>
     <?php Pjax::begin(); ?>    <?= GridView::widget([
         'dataProvider' => $dataProvider,
