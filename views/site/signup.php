2854a4d8eba95b951022400da244eb6c42171cae
doomster <at86@yandex.ru>
2017-11-02 17:54:54 +0200

    українізація

--- views/site/signup.php
+++ views/site/signup.php
@@ -3,19 +3,19 @@
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 
-$this->title = 'Signup';
+$this->title = 'Зареєструватися';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-signup">
     <h1><?= Html::encode($this->title) ?></h1>
-    <p>Please fill out the following fields to signup:</p>
+    <p>Будь ласка, заповніть всі поля для того, щоб зареєструватися на сайті:</p>
     <div class="row">
         <div class="col-lg-5">
 
             <?php $form = ActiveForm::begin(['id' => 'form-signup']); ?>
-            <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
-            <?= $form->field($model, 'email') ?>
-            <?= $form->field($model, 'password')->passwordInput() ?>
+            <?= $form->field($model, 'username')->textInput(['autofocus' => true])->label('Логін') ?>
+            <?= $form->field($model, 'email')->label('Email') ?>
+            <?= $form->field($model, 'password')->passwordInput()->label('Пароль') ?>
             <div class="form-group">
                 <?= Html::submitButton('Signup', ['class' => 'btn btn-primary', 'name' => 'signup-button']) ?>
             </div>
