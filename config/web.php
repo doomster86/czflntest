c0bb6e46daa620e757f26d32c283c2401f156772
doomster <at86@yandex.ru>
2017-12-13 10:38:46 +0200

    timetable start

--- config/web.php
+++ config/web.php
@@ -11,6 +11,7 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'qwerty',
+            'baseUrl'=> '',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
