6ed36d0350d1cc2feef19182586b4392a9e2de73
pavel@fln.com.ua <vjybnjh19pavel@fln.com.ua>
2018-06-20 10:30:03 +0300

    кол-во предметов на страницу у профессии 20

--- controllers/CoursesController.php
+++ controllers/CoursesController.php
@@ -27,7 +27,7 @@ class CoursesController extends Controller {
         if(Yii::$app->user->identity->role==1) {
             $searchModel = new CoursesSearch();
             $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
-            $dataProvider->pagination = ['pageSize' => 20];
+            $dataProvider->pagination = ['pageSize' => 15];
             $dataProvider->sort = [
                 'defaultOrder' => [
                     'name' => SORT_ASC,
@@ -108,7 +108,7 @@ class CoursesController extends Controller {
             $params = Yii::$app->request->queryParams;
             $params['course_id'] = $id;
             $dataProvider = $searchModel->search($params);
-            $dataProvider->pagination = ['pageSize' => 15];
+            $dataProvider->pagination = ['pageSize' => 20];
 
             $model = $this->findModel($id);
 
